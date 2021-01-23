0b7c63094ac79c92735302e28bd95d96b4ad59ba
Andrew Chan <dowinterfor6@gmail.com>
2021-01-23 13:26:49 -0800

    Add basic redis functionality with correct data type

---
+++ README.md
@@ -0,0 +1,4 @@
+# SP500 Companies API
+
+## TODO
+- Validate ticker symbols are in correct format?
--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,14 @@ import { WikiApiParams, WikiParseResponse } from "./interfaces";
 const app = express();
 const redisClient = redis.createClient();
 
-const getAsync = promisify(redisClient.get).bind(redisClient);
-const setAsync = promisify(redisClient.set).bind(redisClient);
+const hmsetAsync = promisify(redisClient.hmset).bind(redisClient);
+const hmgetAsync = promisify(redisClient.hmget).bind(redisClient);
+const flushDbAsync = promisify(redisClient.flushdb).bind(redisClient);
 
-const SP_500_REDIS_KEY: string = "sp500TickerSymbols";
+const SP_500_REDIS_KEY: string = "sp500";
+const SP_500_OLD_REDIS_KEY: string = "sp500Old";
+const DATE_ADDED: string = "dateAdded";
+const TICKER_SYMBOLS: string = "tickerSymbols";
 
 const wikiPageTitle: string = "List_of_S&P_500_companies";
 const url: string = "https://en.wikipedia.org/w/api.php";
@@ -24,75 +28,88 @@ const params: WikiApiParams = {
 };
 
 const fetchFromWiki = async (): Promise<string[]> => {
-  const { data } = await axios.get<WikiParseResponse>(url, {
-    params,
-  });
+  try {
+    const { data } = await axios.get<WikiParseResponse>(url, {
+      params,
+    });
 
-  // TODO: More robust error handling
-  const rawHtml = data.parse?.text;
-  const root = parse(rawHtml);
+    const rawHtml = data.parse.text;
+    const root = parse(rawHtml);
 
-  const sp500Table: HTMLElement =
-    root.querySelector("#constituents") || root.querySelector("table");
+    const sp500Table: HTMLElement =
+      root.querySelector("#constituents") || root.querySelector("table");
 
-  if (!sp500Table) {
-    throw new Error("Error in retrieving table from parsed wiki html");
+    const sp500Root = parse(sp500Table.toString());
+
+    const sp500TableRows: HTMLElement[] = sp500Root.querySelectorAll("tr");
+
+    const sp500TickerSymbols: string[] = [];
+
+    sp500TableRows.forEach((row: HTMLElement, index: number) => {
+      if (index > 0) {
+        // TODO: More elegant solution? row.firstChild gives empty
+        sp500TickerSymbols.push(row.childNodes[1].innerText.replace("\n", ""));
+      }
+    });
+
+    return sp500TickerSymbols;
+  } catch (error) {
+    console.log(`Error fetching from wiki page: ${error}`);
+    return [];
   }
-
-  const sp500Root = parse(sp500Table.toString());
-
-  const sp500TableRows: HTMLElement[] = sp500Root.querySelectorAll("tr");
-
-  const sp500TickerSymbols: string[] = [];
-
-  sp500TableRows.forEach((row: HTMLElement, index: number) => {
-    if (index > 0) {
-      // TODO: More elegant solution? row.firstChild gives empty
-      sp500TickerSymbols.push(row.childNodes[1].innerText.replace("\n", ""));
-    }
-  });
-
-  return sp500TickerSymbols;
 };
+
+const setupRedisCache = async () => {
+  try {
+    await flushDbAsync();
+
+    const wikiData = await fetchFromWiki();
+    if (wikiData.length === 0) return;
+
+    await hmsetAsync(
+      SP_500_REDIS_KEY,
+      DATE_ADDED,
+      Date.now(),
+      TICKER_SYMBOLS,
+      wikiData.toString()
+    );
+    await hmsetAsync(
+      SP_500_OLD_REDIS_KEY,
+      DATE_ADDED,
+      Date.now(),
+      TICKER_SYMBOLS,
+      wikiData.toString()
+    );
+  } catch (error) {
+    console.log(`Error setting up redis cache: ${error}`);
+  }
+};
+
+setupRedisCache();
 
 app.get("/", async (req: Request, res: Response) => {
   try {
-    // TODO: Handle rejection
-    // const redisRes: string | string[] = await getAsync(SP_500_REDIS_KEY);
-    // TODO: Use set to prevent overlap data?
-    redisClient.LRANGE(SP_500_REDIS_KEY, 0, -1, async (err, reply) => {
-      console.log(reply);
-      const redisRes = reply;
+    const cachedData: string[] = await hmgetAsync(
+      SP_500_REDIS_KEY,
+      DATE_ADDED,
+      TICKER_SYMBOLS
+    );
 
-      let sp500TickerSymbols: string[];
+    if (cachedData.some((el) => !el)) {
+      throw new Error("Redis cache was set up incorrectly");
+    }
 
-      // TODO: if key doesn't exist or expired
-      if (reply.length === 0) {
-        sp500TickerSymbols = await fetchFromWiki();
-        // EX is in seconds
-        await redisClient.rpush(SP_500_REDIS_KEY, ...sp500TickerSymbols);
-      } else {
-        sp500TickerSymbols = redisRes;
-      }
-
-      res.status(200).send(sp500TickerSymbols);
-    });
-
-    // let sp500TickerSymbols: string[];
-
-    // // TODO: if key doesn't exist or expired
-    // if (!Array.isArray(redisRes)) {
-    //   sp500TickerSymbols = await fetchFromWiki();
-    //   // EX is in seconds
-    //   await redisClient.rpush(SP_500_REDIS_KEY, ...sp500TickerSymbols);
-    // } else {
-    //   sp500TickerSymbols = redisRes;
-    // }
-
-    // res.status(200).send(sp500TickerSymbols);
+    const formattedData = {
+      dateAdded: cachedData[0],
+      tickerSymbols: cachedData[1].split(","),
+    };
+    res.status(200).json(formattedData);
   } catch (error) {
-    console.log("Error: ", error);
-    res.status(500);
+    console.log(`Error in getting cached data: ${error}`);
+    const errorData = {
+      error,
+    };
+    res.status(500).json(errorData);
   }
 });
 
